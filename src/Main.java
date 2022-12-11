88e7ef1130e5737d0ac0b48d5b3759a88ca8d130
ErnieWhite <ernie.white.68@gmail.com>
2022-12-11 04:50:02 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
